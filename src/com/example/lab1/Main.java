ad2e8210c1d4234857afafaf03cf35894f1d4cdf
dae87 <dae87@192.168.123.101>
2021-09-05 20:21:16 +0900

    1st commit

---
+++ src/com/example/lab1/Main.java
@@ -0,0 +1,20 @@
+package com.example.lab1;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner s = new Scanner(System.in);
+		System.out.print("Hello World!!!\n");
+		System.out.println("");
+		System.out.print("이름 입력 : ");
+		String name = s.next();
+		System.out.println("["+ name +"]님 환영합니다~");
+		
+
+	}
+
+}
