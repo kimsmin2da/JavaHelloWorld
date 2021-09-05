fb6145ecda36addbe0e6d52beeac85add6d8519e
dae87 <dae87@192.168.123.101>
2021-09-05 20:43:45 +0900

    2nd commit

--- src/com/example/lab1/Main.java
+++ src/com/example/lab1/Main.java
@@ -10,7 +10,7 @@ public class Main {
 		Scanner s = new Scanner(System.in);
 		System.out.print("Hello World!!!\n");
 		System.out.println("");
-		System.out.print("이름 입력 : ");
+		System.out.print("Name 입력 : ");
 		String name = s.next();
 		System.out.println("["+ name +"]님 환영합니다~");
 		
